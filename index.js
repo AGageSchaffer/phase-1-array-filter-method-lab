fae950b68ea09e36b53bc85dec93414c0a4baea2
AustinSchaffer <austinschaffer@gmail.com>
2022-12-18 22:51:09 -0600

    commit tree 365a29cd21f3e4f74bbb6463723f017453c12dae

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function filter(collection, cb) {
+    const newCollection = []
+    for (const drivers of collection) {
+        if (cb(drivers)) {
+            newCollection.push(drivers)
+        }
+    }
+    return newCollection
+}
+const findMatching = filter(drivers, function(driver) {
+    return drivers.name === driver
+})
